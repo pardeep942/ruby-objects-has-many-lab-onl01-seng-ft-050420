e16124e99a6e5a4342100daa84e6b20b097ce3a7
PARDEEP KAUR <pkaur84@aol.com>
2020-05-24 22:34:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,34 +1,27 @@
 class Author
-  attr_accessor :name
-  @@post_count = []
+
+  attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def posts
-    @posts
-  end
-
-  def add_post(title)
-    title.author = self
-    @posts << title
-    @@post_count << title
+  def add_post(post)
+    post.author = self
+    @posts << post
   end
 
   def add_post_by_title(title)
-    title = Post.new(title)
-    title.author = self
-    @posts << title
-    @@post_count << title
+    post = Post.new(title)
+    add_post(post)
   end
 
   def self.post_count
-    counter = 0
-    @@post_count.each do |post|
-      counter += 1
-    end
-    counter
+    Post.all.length
   end
-end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+end 
