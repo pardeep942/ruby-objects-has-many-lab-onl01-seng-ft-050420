033b973f951b6649c153bfee8ae3cbe32cb3c1f4
PARDEEP KAUR <pkaur84@aol.com>
2020-05-24 22:20:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,33 @@
 
 class Artist
-
-  attr_accessor :name,
-
-
-  def initialize(name)
+  attr_accessor :name
+  @@all_artists = []
+  def initialize (name)
     @name = name
+    @songs =[]
+    @@all_artists << self
   end
 
   def add_song(song)
-    @songs << song
+
     song.artist = self
-    @@song_count += 1
   end
 
-  def add_song_by_name(title)
-    song = Song.new(title)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song (song)
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 
   def self.song_count
-    @@song_count
+    song_count=0
+    @@all_artists.each do |artist|
+      array = artist.songs
+      song_count += array.length
+     end
+    song_count
   end
-
 end
