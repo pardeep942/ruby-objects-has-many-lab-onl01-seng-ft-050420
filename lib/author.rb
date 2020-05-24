ee05ea41065912db09b2b407731db1912a64640e
PARDEEP KAUR <pkaur84@aol.com>
2020-05-24 21:35:27 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+  attr_accessor :name
+
+def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+def Song
+  @Song
+end
+def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end 
+  
