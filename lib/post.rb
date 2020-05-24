f61054367a42b1adf08bf35d1072343c793e6aa7
PARDEEP KAUR <pkaur84@aol.com>
2020-05-24 21:59:09 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,27 +1,15 @@
 class Post
+
   attr_accessor :title, :author
-
-  @@all = []
-
-  def initialize (title)
+  def initialize(title)
     @title = title
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def author_name
-    if author == nil
-      nil
+    if @author == nil
+      return nil
     else
-      self.author.name
+      @author.name
     end
   end
-
-  def add_post(post)
-    post.author = self
-  end
-
 end
